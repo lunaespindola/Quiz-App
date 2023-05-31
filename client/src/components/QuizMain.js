4fbd9c4b4848d6bb8a11f9b27796ec62053792d3
“Alan <A01751117@tec.mx>
2023-05-30 22:55:26 -0600

    Changed ip

--- client/src/components/QuizMain.js
+++ client/src/components/QuizMain.js
@@ -28,7 +28,7 @@ export default class Quiz extends Component {
     const numQuestions = localStorage.getItem("numQuestions");
 
     if (localStorage.getItem("logged") === "true") {
-      axios.post("http://100.26.101.193 :5000/api/getQuestions", {
+      axios.post("http://100.26.101.193:8080/api/getQuestions", {
         numQuestions
         })
         .then(
@@ -114,7 +114,7 @@ export default class Quiz extends Component {
     const username = localStorage.getItem("username");
     localStorage.setItem("logged", false);
     axios
-      .post("http://100.26.101.193 :5000/api/Addscores", {
+      .post("http:///api/Addscores", {
         username,
         score,
       })
--- client/src/pages/RegisterPages.js
+++ client/src/pages/RegisterPages.js
@@ -30,7 +30,7 @@ const RegisterPages = () => {
 
   const onRegister = (e) => {
     e.preventDefault();
-    axios.post("http://100.26.101.193 :5000/api/users", {
+    axios.post("http:///api/users", {
       username,
       numQuestions,
     }).then
