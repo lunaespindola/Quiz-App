9989128048e2ce1aee7bd81a1d4614e05c5b22f9
“Alan <A01751117@tec.mx>
2023-05-30 21:20:21 -0600

    Changed ip

--- client/src/pages/RegisterPages.js
+++ client/src/pages/RegisterPages.js
@@ -30,7 +30,7 @@ const RegisterPages = () => {
 
   const onRegister = (e) => {
     e.preventDefault();
-    axios.post("http://127.0.0.1:5000/api/users", {
+    axios.post("http://44.204.48.100:5000/api/users", {
       username,
       numQuestions,
     }).then
