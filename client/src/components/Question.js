1f2a0964edd7578185aa88660b5479fec311acda
Luna Espindola <Lunaespindola04@gmail.com>
2024-11-28 10:40:54 -0600

    changed deadname

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -2,7 +2,7 @@
   Date: 30/05/2023
   Authors:
             A01750624 Paulina Guadalupe Alva Martinez
-            A01751117 Carlos Alan Gallegos Espinola
+            A01751117 Luna Abril Gallegos Espinola
             A01378450 Jorge Alberto Penagos Mendez
             A01750363 Naomi Anciola Calderon */
 
